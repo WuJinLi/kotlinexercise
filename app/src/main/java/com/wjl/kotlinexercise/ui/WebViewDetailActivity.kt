358a0e300c6a07b051056feeb5a9606672e5acaf
wujinli <269316671@qq.com>
2018-09-11 23:46:44 +0800

    banner轮播图

--- app/src/main/java/com/wjl/kotlinexercise/ui/WebViewDetailActivity.kt
+++ app/src/main/java/com/wjl/kotlinexercise/ui/WebViewDetailActivity.kt
@@ -1,18 +1,26 @@
 package com.wjl.kotlinexercise.ui
 
-import android.webkit.WebChromeClient
-import android.webkit.WebSettings
-import android.webkit.WebViewClient
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.*
 import com.wjl.kotlinexercise.R
 import com.wjl.kotlinexercise.base.BaseActivity
 import kotlinx.android.synthetic.main.ac_webview_details.*
+import kotlin.math.log
 
 /**
  * @author: wjl
  * @date:2018/9/11
  */
 class WebViewDetailActivity : BaseActivity() {
-    var url:String=""
+    private var url: String = ""
+
+
+    companion object {
+        val TAG = WebViewDetailActivity::class.java.simpleName
+    }
+
     override fun attachLayoutRes(): Int = R.layout.ac_webview_details
 
     override fun initData() {
@@ -26,14 +34,15 @@ class WebViewDetailActivity : BaseActivity() {
                 javaScriptEnabled = true
                 javaScriptCanOpenWindowsAutomatically = true
                 allowContentAccess = true
-                allowFileAccess=true
+                allowFileAccess = true
                 setSupportZoom(true)
                 cacheMode = WebSettings.LOAD_NO_CACHE
                 domStorageEnabled = true
                 databaseEnabled = true
             }
-            webChromeClient = WebChromeClient()
-            webViewClient = WebViewClient()
+            webChromeClient = wcClient
+            webViewClient = wvClient
+            setOnKeyListener(onKeyEvent)
         }
 
         ww_webview.loadUrl(url)
@@ -43,4 +52,30 @@ class WebViewDetailActivity : BaseActivity() {
 
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    private val onKeyEvent = View.OnKeyListener { v, keyCode, event ->
+        val action = event.action
+        val webview = v as WebView
+        if (KeyEvent.ACTION_DOWN == action && KeyEvent.KEYCODE_BACK == keyCode) {
+            if (webview?.canGoBack()) {
+                webview.goBack()
+                return@OnKeyListener true
+            }
+        }
+        false
+    }
+
+    private val wcClient = object : WebChromeClient() {
+
+    }
+
+    private val wvClient = object : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            Log.e(TAG, url)
+            return false
+        }
+    }
 }
--- app/src/main/java/com/wjl/kotlinexercise/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/wjl/kotlinexercise/ui/fragment/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ImageView
+import cn.bingoogolapple.bgabanner.BGABanner
+import com.bumptech.glide.Glide
 import com.wjl.kotlinexercise.adapter.BannerListAdapter
 import com.wjl.kotlinexercise.R
 import com.wjl.kotlinexercise.base.BaseFragment
@@ -12,7 +15,9 @@ import com.wjl.kotlinexercise.mvp.model.Banner
 import com.wjl.kotlinexercise.mvp.model.HomeBean
 import com.wjl.kotlinexercise.mvp.presenter.HomePresenter
 import com.wjl.kotlinexercise.ui.WebViewDetailActivity
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.fg_home.*
+import kotlinx.android.synthetic.main.fg_home.view.*
 
 /**
  * author: WuJinLi
@@ -21,14 +26,11 @@ import kotlinx.android.synthetic.main.fg_home.*
  */
 class HomeFragment : BaseFragment(), HomeContract.View {
 
-    var adapter: BannerListAdapter? = null
-    override fun setData(bean: List<Banner>) {
-        adapter!!.addData(bean)
-    }
-
-    override fun setHomeData(bean: HomeBean) {
-    }
-
+    private var adapter: BannerListAdapter? = null
+    /**
+     * banner datas
+     */
+    private lateinit var bannerDatas: ArrayList<Banner>
 
     companion object {
         fun getInstance(): HomeFragment = HomeFragment()
@@ -54,10 +56,58 @@ class HomeFragment : BaseFragment(), HomeContract.View {
             }
 
         })
+
+        br_banner.run {
+            setDelegate(bannerDelegate)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         mPresenter.getBannerData()
+    }
+
+
+    override fun setData(banners: List<Banner>) {
+//        adapter!!.addData(bean)
+        bannerDatas = banners as ArrayList<Banner>
+        val bannerFeedList = ArrayList<String>()
+        val bannerTitleList = ArrayList<String>()
+        Observable.fromIterable(banners)
+                .subscribe({ list ->
+                    bannerFeedList.add(list.imagePath)
+                    bannerTitleList.add(list.title)
+                })
+
+        br_banner?.run {
+            setAutoPlayAble(bannerFeedList.size > 1)
+            setData(bannerFeedList, bannerTitleList)
+            setAdapter(bannerAdapter)
+        }
+    }
+
+    override fun setHomeData(bean: HomeBean) {
+    }
+
+    /**
+     * Banner Adapter
+     */
+    private val bannerAdapter: BGABanner.Adapter<ImageView, String> by lazy {
+        BGABanner.Adapter<ImageView, String> { bgaBanner, imageView, feedImageUrl, position ->
+            Glide.with(activity!!).load(feedImageUrl).into(imageView)
+        }
+    }
+
+    /**
+     * BannerClickListener
+     */
+    private val bannerDelegate = BGABanner.Delegate<ImageView, String> { banner, imageView, model, position ->
+        if (bannerDatas.size > 0) {
+            val data = bannerDatas[position]
+            Intent(activity, WebViewDetailActivity::class.java).run {
+                putExtra("url", data.url)
+                startActivity(this)
+            }
+        }
     }
 }
